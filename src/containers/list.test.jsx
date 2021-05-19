0fde75e3bd8272f0584d4895034262d473f4740d
Vance-M <myveryvills@yahoo.com>
2021-05-19 11:56:02 -0700

    fix MemoryRouter

--- src/containers/list.test.jsx
+++ src/containers/list.test.jsx
@@ -4,7 +4,7 @@ import { rest, } from 'msw';
 import { setupServer } from 'msw/node';
 import RickandMorty from './RandMFN';
 import mockData from '../services/mockData.json'
-import App from '../components/app/App';
+import { MemoryRouter } from 'react-router';
 
 const server = setupServer(
     rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
@@ -19,7 +19,11 @@ describe('Rick and Morty Character container', async() => {
     beforeAll(() => server.listen());
     afterAll(() => server.close());
     it('fetches a list of Rick and Morty characters', async () => {
-        render(<App />);
+        render(
+            <MemoryRouter>
+                <RickandMorty />           
+            </MemoryRouter>
+        );
 
         screen.getByText('Loading...');
 
